d4793988f51b929876fcc1b0774e6c8aeb4d81c3
ekmartin <ekmartinb@gmail.com>
2014-05-29 21:09:28 +0200

    Make it work with private messages, add env variables for stuff and refactor.

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
+#!/usr/bin/env node
+
+var weechat = require('weechat'),
+    notify  = require('osx-notifier'),
+    client;
+
 var properties = {
-  server:   '',
-  port:     9000,
-  password: '',
+  server:   process.env.SERVER,
+  port:     8001,
+  password: process.env.PASSWORD,
   ssl:      false,
-  nick:     ''
+  nicks:    process.env.NICKS.split(',')
 };
 
-var weechat = require('weechat');
-var notify = require('osx-notifier');
-
-var raise_notification = function(from, message) {
-
+var raiseNotification = function(from, message) {
   notify({
     type: 'info',
-    title: 'Weechat',
+    title: 'WeeChat',
     subtitle: from,
     message: message,
-    group: 'Weechat'
+    group: 'WeeChat'
   });
-
 };
 
-var client = weechat.connect(properties.server, properties.port, properties.password, properties.ssl, function() {
-
-  client.on('error', function(err) {
-    console.error(err);
+var connect = function() {
+  return weechat.connect(properties.server, properties.port, properties.password, properties.ssl, function() {
+    console.log('Connected.');
   });
+};
 
-  client.on('end', function() {
-    console.log('end');
+client = connect();
+
+client.on('line', function(line) {
+  var from = weechat.noStyle(line.prefix);
+  var message = weechat.noStyle(line.message);
+
+  var containsNick = properties.nicks.some(function(nick) {
+    return message.indexOf(nick) !== -1;
   });
+  var isPrivate = line.tags_array.indexOf('notify_private') !== -1;
+  var isSelf = from === properties.nick;
 
-  client.on('line', function(line) {
-    console.log(line);
-    var from = weechat.noStyle(line.prefix);
-    var message = weechat.noStyle(line.message);
-
-    if (message.indexOf(properties.nick) >= 0 && from.indexOf(properties.nick) == -1){
-      raise_notification(from, message);
-    }
-  });
-
+  // Make sure the message is either a highlight or a PM:
+  if ((!isSelf && containsNick) || isPrivate) {
+    raiseNotification(from, message);
+  }
 });
-
