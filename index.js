218f3803a2464122cfd1064c04464901153a5b9e
ekmartin <ekmartinb@gmail.com>
2014-05-29 22:25:54 +0200

    Make sure it only sends notifications on highlights, pms and mode changes (op/deop of yourself etc.)

--- index.js
+++ index.js
@@ -34,16 +34,23 @@ client.on('line', function(line) {
   var from = weechat.noStyle(line.prefix);
   var message = weechat.noStyle(line.message);
 
+  if (['@', '+'].indexOf(from[0]) !== -1) {
+    from = from.slice(1);
+  }
+
   var containsNick = properties.nicks.some(function(nick) {
     return message.indexOf(nick) !== -1;
   });
   var isSelf = properties.nicks.some(function(nick) {
+    console.log("checking", nick, from);
     return nick === from;
   });
   var isPrivate = line.tags_array.indexOf('notify_private') !== -1;
+  var isValid = line.tags_array.indexOf('irc_mode') !== -1
+      || line.tags_array.indexOf('notify_message') !== -1;
 
   // Make sure the message is either a highlight or a PM:
-  if ((!isSelf && containsNick) || isPrivate) {
+  if ((!isSelf && containsNick && isValid) || isPrivate) {
     raiseNotification(from, message);
   }
 });
