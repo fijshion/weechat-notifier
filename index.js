cd5d86d3f40ef2a6c4d1f38899d9ed1a6f33fe05
Chris Kyrouac <ckyrouac@redhat.com>
2016-09-02 13:01:07 -0400

    Switch to node-notifier and read opts with yargs

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 #!/usr/bin/env node
 
-var weechat = require('weechat'),
-    notify  = require('osx-notifier'),
-    client;
+var weechat = require('weechat');
+var notifier = require('node-notifier');
+var client;
+var argv = require('yargs').argv;
+
+if (!argv.server || !argv.port || !argv.password || !argv.nicks) {
+  console.error(
+    'Usage: node index.js --server=<server> --port=<port> ' +
+    '--password=<password> --nicks=<nicks> --ssl=<true|false>');
+  return 0;
+}
 
 var properties = {
-  server:   process.env.SERVER,
-  port:     process.env.PORT,
-  password: process.env.PASSWORD,
-  ssl:      false,
-  nicks:    process.env.NICKS.split(',')
+  server: argv.server,
+  port: argv.port,
+  password: argv.password,
+  ssl: argv.ssl,
+  nicks: argv.nicks.split(',')
 };
 
 var raiseNotification = function(from, message) {
-  notify({
-    type: 'info',
+  notifier.notify({
     title: 'WeeChat',
     subtitle: from,
     message: message,
-    group: 'WeeChat'
   });
 };
 
 var connect = function() {
-  return weechat.connect(properties.server, properties.port, properties.password, properties.ssl, function() {
-    console.log('Connected.');
-  });
+  return weechat.connect(
+    properties.server,
+    properties.port,
+    properties.password,
+    properties.ssl,
+    function() {
+      console.log('Connected.');
+      notifier.notify({
+        title: 'Weechat Relay Connected.',
+        message: ''
+      });
+    });
 };
 
 client = connect();
@@ -42,12 +57,11 @@ client.on('line', function(line) {
     return message.indexOf(nick) !== -1;
   });
   var isSelf = properties.nicks.some(function(nick) {
-    console.log("checking", nick, from);
     return nick === from;
   });
   var isPrivate = line.tags_array.indexOf('notify_private') !== -1;
-  var isValid = line.tags_array.indexOf('irc_mode') !== -1
-      || line.tags_array.indexOf('notify_message') !== -1;
+  var isValid = line.tags_array.indexOf('irc_mode') !== -1 ||
+    line.tags_array.indexOf('notify_message') !== -1;
 
   // Make sure the message is either a highlight or a PM:
   if ((!isSelf && containsNick && isValid) || isPrivate) {
